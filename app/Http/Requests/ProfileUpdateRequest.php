a809768f7fcf7f8bffc9069415c0f8fe78abc967
Bozier Luan <luan.bozier@he-arc.ch>
2024-10-30 19:08:18 +0100

    fix profile update

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules;
 use Illuminate\Validation\Rule;
 
-class ProfileRequest extends FormRequest
+class ProfileUpdateRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
@@ -17,9 +17,10 @@ class ProfileRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'username' => ['required', 'string', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'username' => ['required', 'string', 'max:255', Rule::unique(User::class)->ignore($this->user())],
             'filiere'  => ['required', 'string', 'max:10'],
             'year'     => ['required', 'integer', 'min:1', 'max:3'],
+            'profile_picture_type' => ['required', 'integer']
         ];
     }
 }
