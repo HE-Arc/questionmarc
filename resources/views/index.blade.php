8f5390b5c6d7ebf8feb0d915e8a80669668a5f58
Vivone Tom <tom.vivone@he-arc.ch>
2024-10-30 10:10:19 +0100

    Refactor question index view and controller. Update author avatar and display creation date.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,14 @@
         <div class="grid grid-cols-1 gap-6 max-w-xl flex-1 p-6">
             @foreach ($questions as $question)
                 <div class="bg-white shadow-md rounded-lg p-6 flex-1">
-                    <div class="flex items-center mb-4">
-                        <div class="text-sm font-medium text-gray-900">{{ $question->author->username}}</div>
+                    <div class="flex items-center gap-4 mb-4">
+                        <div class="relative w-8 h-8 bg-gray-100 rounded-full flex items-center justify-center overflow-hidden">
+                            <img src="https://robohash.org/{{$question->author->username}}.png?size=50x50" class="w-full h-full object-cover">
+                        </div>
+                        <div class="ml-2 font-medium dark:text-black">
+                            <div>{{ $question->author->username }}</div>
+                            <div class="text-sm text-gray-500">Créé le {{ $question->created_date }}</div>
+                        </div>
                     </div>
                     <div class="mb-2">
                         <h3 class="text-lg font-bold text-gray-900">{{ $question->title }}</h3>
